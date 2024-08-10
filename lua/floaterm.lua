1150cc464e7870450a00f04dc67ab080625f3c1b
nikibeep <84743235+nikibeep@users.noreply.github.com>
2024-08-11 00:09:56 +0530

    Untrack all files

--- lua/floaterm.lua
+++
@@ -1,9 +0,0 @@
--- ~/.config/nvim/lua/floaterm.lua
-
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-
--- FloaTerm configuration
-map('n', '<leader>ft', ':FloatermNew --name=myfloat --height=0.8 --width=0.7 --autoclose=2 fish<CR>', opts)
-map('n', 't', ':FloatermToggle myfloat<CR>', opts)
-map('t', '<Esc>', '<C-\\><C-n>:q<CR>', opts)
